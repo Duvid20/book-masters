13991869203bc2af4cde1ebc59ae6d28f3aa1645
Duvid20 <{ID}+{username}@users.noreply.github.com>
2024-08-17 18:30:32 +0200

    Refactor database connection code to host locally, delete .gitignore

--- includes/db.php
+++ includes/db.php
@@ -2,14 +2,16 @@
 
 function getConnection()
 {
-    $servername = "";
-    $username = "";
+    $servername = "localhost";
+    $username = "root";
     $password = "";
-    $dbname = "";
+    $dbname = "book_masters";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
+
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+
     return $conn;
 }
