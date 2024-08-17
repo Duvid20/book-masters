b5e9b121575a06fafc72f6a26e1859239ef58c88
Duvid20 <{ID}+{username}@users.noreply.github.com>
2024-08-17 17:08:46 +0200

    chore: Refactor database connection code to use environment variables

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,15 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "password";
-$dbname = "your_database";
+function getConnection()
+{
+    $servername = getenv('DB_SERVERNAME');
+    $username = getenv('DB_USERNAME');
+    $password = getenv('DB_PASSWORD');
+    $dbname = getenv('DB_NAME');
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
 }
